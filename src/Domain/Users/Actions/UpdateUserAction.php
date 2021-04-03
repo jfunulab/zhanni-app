1a7329c455d91cd433ff7aed1870a68e00695eca
jfunulab <jfunu79@gmail.com>
2021-04-03 15:51:23 +0100

    preventing override of data when updating user.

--- src/Domain/Users/Actions/UpdateUserAction.php
+++ src/Domain/Users/Actions/UpdateUserAction.php
@@ -13,9 +13,9 @@ class UpdateUserAction
     public function __invoke(User $user, UserData $userData): User
     {
         $user->fill([
-            'first_name' => $userData->firstName,
-            'last_name' => $userData->lastName,
-            'phone_number' => $userData->phoneNumber
+            'first_name' => $userData->firstName ?? $user->first_name,
+            'last_name' => $userData->lastName ?? $user->last_name,
+            'phone_number' => $userData->phoneNumber ?? $user->phone_number
         ])->save();
 
         return $user;
