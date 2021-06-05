d5441ddfcf6f72e56d3e2410130843b50b03cebb
Joel Funu <jfunu79@gmail.com>
2021-06-05 07:25:16 +0000

    updated horizon config

--- config/horizon.php
+++ config/horizon.php
@@ -191,6 +191,13 @@ return [
                 'balanceCooldown' => 3,
             ],
         ],
+        'development' => [
+            'supervisor-1' => [
+                'maxProcesses' => 10,
+                'balanceMaxShift' => 1,
+                'balanceCooldown' => 3,
+            ],
+        ],
         'local' => [
             'supervisor-1' => [
                 'maxProcesses' => 3,
